06b75950c3fddd779b43a974e4501731c625c5f5
hansmel <64403277+hansmel@users.noreply.github.com>
2020-07-08 17:19:13 +0200

    Add write to users.txt file

--- fileio/filewriter.go
+++ fileio/filewriter.go
@@ -1,6 +1,7 @@
 package fileio
 
 import (
+	"os"
 	"fmt"
 
 	"github.com/pluralsight/webservice/models"
@@ -9,5 +10,26 @@ import (
 // FileWriter writes data to file
 func FileWriter(user models.User) {
 	fmt.Println("Filewriter")
+
+	f, err := os.Create("users.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = f.WriteString(user.FirstName + " " + user.LastName + "\n")
+	if err != nil {
+		fmt.Println(err)
+		f.Close()
+		return
+	}
+
+	err = f.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	fmt.Println(user)
 }
+
