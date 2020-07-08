55d663e8e4d29d6d488968dd8ba4132ce6340189
hansmel <64403277+hansmel@users.noreply.github.com>
2020-07-08 17:02:42 +0200

    Add debug prints and comments

--- models/user.go
+++ models/user.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 )
 
+// User is the container for user data
 type User struct {
 	ID        int
 	FirstName string
@@ -16,11 +17,14 @@ var (
 	nextID = 1
 )
 
+// GetUsers returns all users
 func GetUsers() []*User {
 	return users
 }
 
+// AddUser adds a user to the list of users
 func AddUser(u User) (User, error) {
+	fmt.Println("models.AddUser")
 	if u.ID != 0 {
 		return User{}, errors.New("New User must not include id or it must be set to zero")
 	}
@@ -30,7 +34,9 @@ func AddUser(u User) (User, error) {
 	return u, nil
 }
 
+// GetUserByID returns the user with the provided id
 func GetUserByID(id int) (User, error) {
+	fmt.Println("models.GetUserByID")
 	for _, u := range users {
 		if u.ID == id {
 			return *u, nil
@@ -40,7 +46,9 @@ func GetUserByID(id int) (User, error) {
 	return User{}, fmt.Errorf("User with ID '%v' not found", id)
 }
 
+// UpdateUser updates the user wtih the provided user id
 func UpdateUser(u User) (User, error) {
+	fmt.Println("models.UpdateUser")
 	for i, candidate := range users {
 		if candidate.ID == u.ID {
 			users[i] = &u
@@ -51,7 +59,9 @@ func UpdateUser(u User) (User, error) {
 	return User{}, fmt.Errorf("User with ID '%v' not found", u.ID)
 }
 
-func RemoveUserById(id int) error {
+// RemoveUserByID removes the user with the provided user id from the list of users
+func RemoveUserByID(id int) error {
+	fmt.Println("models.RemoveUserById")
 	for i, u := range users {
 		if u.ID == id {
 			users = append(users[:i], users[i+1:]...)
