57adf95f314c3cd01cd15f7ea732b2a603a1ad80
hansmel <64403277+hansmel@users.noreply.github.com>
2020-07-08 17:21:08 +0200

    Update comment

--- fileio/filewriter.go
+++ fileio/filewriter.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pluralsight/webservice/models"
 )
 
-// FileWriter writes data to file
+// FileWriter writes user data to file
 func FileWriter(user models.User) {
 	fmt.Println("Filewriter")
 
